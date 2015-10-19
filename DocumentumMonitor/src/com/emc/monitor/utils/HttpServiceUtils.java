fd428942c0045aa5c08a803eb0e8f85fd34928e9
levin1 <levin1@RUCSLEVIN1L1C.corp.emc.com>
2015-10-19 14:07:15 +0300

    .

--- DocumentumMonitor/src/com/emc/monitor/utils/HttpServiceUtils.java
+++ DocumentumMonitor/src/com/emc/monitor/utils/HttpServiceUtils.java
@@ -3,6 +3,9 @@ package com.emc.monitor.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.http.HttpHost;
@@ -19,76 +22,53 @@ import org.apache.tomcat.util.codec.binary.Base64;
 
 public class HttpServiceUtils {
 
-	public static String sendRequest(String host, int port, String protocol, String pinfo, String user,
-			String password) {
-		String result = null;
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-
+	public static String sendRequest(String host, int port, String protocol, String pinfo, String user, String password)
+			throws IOException {
+		String result = null;		
+		URL url = null;
 		try {
-			HttpClientContext context = HttpClientContext.create();
-			CookieStore cookieStore = new BasicCookieStore();
-			context.setAttribute(HttpClientContext.COOKIE_STORE, cookieStore);
-			HttpHost targetHost = new HttpHost(host, port, protocol);
-			HttpGet request = new HttpGet(pinfo);
-			if (user != null && password != null) {
-				request.addHeader("Authorization", "Basic " + getEncodedCredentials(user, password));
-			}
-			request.addHeader("Accept", "text/html,application/xml,*/*");
-
-			System.out.println("Executing request " + request + " to " + targetHost);
-
-			CloseableHttpResponse response = null;
-			int responseStatusCode = 0;
-			try {
-				response = httpclient.execute(targetHost, request, context);
-			} catch (IOException e) {
-				 System.out.println("Cannot connect to " + targetHost);
-				responseStatusCode = 1;
-			} finally {
-				if (responseStatusCode == 1) {
-					result = "Failed";
-				} else {
-					responseStatusCode = response.getStatusLine().getStatusCode();
-					 System.out.println("GET Response Status:: " +
-					 responseStatusCode);
-					if (responseStatusCode == 259 || responseStatusCode == 200) {
-
-						BufferedReader br = new BufferedReader(
-								new InputStreamReader(response.getEntity().getContent()));
-						String inputLine;
-						StringBuffer sb = new StringBuffer();
-						while ((inputLine = br.readLine()) != null) {
-							sb.append(inputLine);
-						}
-						result = sb.toString();
-						 System.out.println("Response: " + result);
-					} else {
-						result = "Failed";
-					}
-
-					EntityUtils.consume(response.getEntity());
-					response.close();
-				}
-
-			}
-		} catch (ClientProtocolException e1) {
+			url = new URL(protocol.concat("://").concat(host).concat(":").concat(Integer.toString(port)).concat(pinfo));
+		} catch (MalformedURLException e1) {
 			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		} finally {
-			try {
-				httpclient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-		 System.out.println("Response: " + result);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestProperty("Accept", "text/html,application/xml,*/*");
+
+		if (user != null && password != null) {
+			con.setRequestProperty("Authorization", "Basic " + getEncodedCredentials(user, password));
+		}
+		System.out.println("Executing request to " + url.toString());
+		int responseStatusCode = 0;
+		try {
+			responseStatusCode = con.getResponseCode();
+		} catch (IOException e) {
+			System.out.println("Cannot connect to " + url.toString());
+			responseStatusCode = 1;
+		} finally {
+			if (responseStatusCode == 1) {
+				result = "Failed";
+			} else {
+				System.out.println("GET Response Status:: " + responseStatusCode);
+				if (responseStatusCode == 259 || responseStatusCode == 200) {
+
+					BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					String inputLine;
+					StringBuffer response = new StringBuffer();
+					while ((inputLine = br.readLine()) != null) {
+						response.append(inputLine);
+					}
+					result = response.toString();
+					System.out.println("Response: " + result);
+				} else {
+					result = "Failed";
+				}
+			}
+		}		
 		return result;
 	}
 
-	private static String getEncodedCredentials(String user, String password) {		
-		String encodedPwd = new String(
-				Base64.encodeBase64(user.concat(":").concat(password).getBytes()),
+	private static String getEncodedCredentials(String user, String password) {
+		String encodedPwd = new String(Base64.encodeBase64(user.concat(":").concat(password).getBytes()),
 				StandardCharsets.UTF_8);
 		return encodedPwd;
 	}
