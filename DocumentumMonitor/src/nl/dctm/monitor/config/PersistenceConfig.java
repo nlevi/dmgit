c69dfc2ce421b9f9c77aef6b429f046c925153b1
nlevi <nikita.levi@gmail.com>
2016-02-04 11:23:51 +0300

    .

--- DocumentumMonitor/src/nl/dctm/monitor/config/PersistenceConfig.java
+++ DocumentumMonitor/src/nl/dctm/monitor/config/PersistenceConfig.java
@@ -1,5 +1,21 @@
 package nl.dctm.monitor.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
@@ -7,4 +23,49 @@ package nl.dctm.monitor.config;
 @ComponentScan( {"nl.dctm.monitor"} )
 public class PersistenceConfig {
 
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(restDataSource());
+		sessionFactory.setPackagesToScan(new String[] { "nl.dctm.monitor" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+	
+	@Bean
+	public DataSource restDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driver"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUrl(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		return dataSource;
+	}
+	
+	Properties hibernateProperties() {  
+	     return new Properties() {  
+	        {  
+	           setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));  
+	           setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));  
+	           setProperty("hibernate.globally_quoted_identifiers", "true");  
+	        }  
+	     };  
+	  }  
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+		return txManager;
+	}
+	
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslaction() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+			
 }
